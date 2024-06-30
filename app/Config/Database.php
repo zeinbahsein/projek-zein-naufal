ea07d6a066f6f457bf3327bc57df44acb6c42757
NopelPunya <hasburrahmannaufal@gmail.com>
2024-06-30 14:59:37 +0700

    memperbarui kode untuk error logic

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'fitnes',
+        'database'     => 'fitnesrev',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => 3307,
         'numberNative' => false,
         'dateFormat'   => [
             'date'     => 'Y-m-d',
@@ -177,7 +177,7 @@ class Database extends Config
         'compress'    => false,
         'strictOn'    => false,
         'failover'    => [],
-        'port'        => 3306,
+        'port'        => 3307,
         'foreignKeys' => true,
         'busyTimeout' => 1000,
         'dateFormat'  => [
